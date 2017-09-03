cfb21b49244e333ad34a5c1ba7c45ba02ab71b4e
Paulo Oliveira <pauloadoliveira@gmail.com>
2017-09-03 11:40:57 -0300

    config

--- spring/mysqlJpa/src/main/java/com/usecases/spring/MysqlJpaApplication.java
+++ spring/mysqlJpa/src/main/java/com/usecases/spring/MysqlJpaApplication.java
@@ -2,8 +2,10 @@ package com.usecases.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan(basePackageClasses = {MysqlJpaApplication.class, Commons.class})
 public class MysqlJpaApplication {
